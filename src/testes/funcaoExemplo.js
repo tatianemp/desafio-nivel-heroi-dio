a6837d7fc88e2e7c928ca518762005e3f3508c03
tatianemp <tatiane.maris@gmail.com>
2026-07-06 21:28:06 -0300

    calculadora partidas

--- src/testes/funcaoExemplo.js
+++
@@ -1,5 +0,0 @@
-createDataBase("db_products", "tatiane", "123345")
-
-function createDataBase (databaseName, username, password) {
-    console.log(`connect:DBCONNECT;user=${username};pass=${password};initial_database= ${databaseName}`);
-}
--- src/testes/retorno.js
+++
@@ -1,8 +0,0 @@
-let resultado = soma(10, 20);
-console.log("O resultado da soma é: " + resultado);
-
-function soma(numA, numB) {
-    let somatorio = numA + numB;
-    return somatorio;
-
-}
